a58df1fa41851ccc78dc530fc83a501901ec9370
ritu.infinity <ritu.jaiswal@thewitslab.com>
2026-02-12 13:39:06 +0530

    first commit

---
+++ src/components/parent.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+
+export default function Parent() {
+  const [name, setName] = useState("Ritu");
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Parent Component</h2>
+      <p>Name: {name}</p>
+
+      <button onClick={() => setName("Aman")}>Change Name</button>
+
+      {/* Passing props to child */}
+      <Child name={name} />
+    </div>
+  );
+}
+
+// Child Component
+function Child(props:any) {
+  return (
+    <div style={{ border: "1px solid black", padding: "10px", marginTop: "10px" }}>
+      <h3>Child Component</h3>
+      <p>Name: {props.name}</p>
+    </div>
+  );
+}
+
+// Parent Component
